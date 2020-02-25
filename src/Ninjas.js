cb89fdbd98a569c99eb571ca5abfa61db952a88e
Furtuna Laurentiu <virgil.furtuna@gmail.com>
2020-02-25 08:44:03 +0200

    Add a new component

---
+++ src/Ninjas.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Ninjas() {
+    return (
+      <div className="ninja">
+          <div>Name: Furtuna</div>
+          <div>Age: 28</div>
+          <div>Belt: Black</div>
+      </div>
+    );
+  }
+
+  export default Ninjas;
