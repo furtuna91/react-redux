a8a2ee0d1c6ab9b3bc417e5a47f237661cd1032d
Furtuna Laurentiu <virgil.furtuna@gmail.com>
2020-02-27 08:40:48 +0200

    Conditional format

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -3,14 +3,23 @@ import React from 'react';
 const Ninjas = ({ ninjas }) =>  {
     // const { ninjas } = props;
     
+    // const ninjaList = ninjas.map(ninja => {
+    //     return (
+    //         <div className="ninja" key={ninja.id}>
+    //             <div>Name: { ninja.name }</div>
+    //             <div>Age: { ninja.age }</div>
+    //             <div>Belt: { ninja.belt }</div>
+    //         </div>
+    //     )
+    // })
     const ninjaList = ninjas.map(ninja => {
-        return (
+        return ninja.age > 25 ? (
             <div className="ninja" key={ninja.id}>
                 <div>Name: { ninja.name }</div>
                 <div>Age: { ninja.age }</div>
                 <div>Belt: { ninja.belt }</div>
             </div>
-        )
+        ) : null;
     })
     return (
         <div className="ninja-list">
